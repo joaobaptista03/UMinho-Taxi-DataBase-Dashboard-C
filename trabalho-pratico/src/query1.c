3552baf0bbc9d6c2e0c85e661eb6952b80c8e3bb
a100705 <a100705@alunos.uminho.pt>
2022-11-16 14:34:25 +0000

    query1 corrigida (taxas)

--- trabalho-pratico/src/query1.c
+++ trabalho-pratico/src/query1.c
@@ -11,7 +11,18 @@ void query1 (int counter, Driver *drivers_cat, User *users_cat, Ride *rides_cat,
         if (isDriver(id, strlen(id))) {
             int id_driver = atoi(id);                           // Converter string id do input para int
             Driver q1_d = drivers_cat[id_driver];               // Ir buscar o driver pretendido pelo ID ao Catálogo
-            
+
+            // Calcula a taxa base e a taxa por distância de acordo com a classe do carro
+            float taxa_base;
+            if (strcpy(q1_d.car_class, "basic")) taxa_base = 3.25;
+                else if (strcmp(q1_d.car_class, "green") == 0) taxa_base = 4;
+                else if (strcmp(q1_d.car_class, "premium") == 0) taxa_base = 5.2;
+            float taxa_dist;
+            if (strcpy(q1_d.car_class, "basic")) taxa_dist = 0.62;
+                else if (strcmp(q1_d.car_class, "green") == 0) taxa_dist = 0.79;
+                else if (strcmp(q1_d.car_class, "premium") == 0) taxa_dist = 0.94;
+
+
             char avaliacao_media[15]; int total_avaliacoes = 0; double av_media = 0;
             char numero_viagens[10]; int num_viagens = 0;
             char total_auferido[15]; double tot_auferido = 0;
@@ -21,7 +32,7 @@ void query1 (int counter, Driver *drivers_cat, User *users_cat, Ride *rides_cat,
                 if (strcmp(rides_cat[i].driver, id) == 0) {
                     total_avaliacoes += atoi(rides_cat[i].score_driver);
                     num_viagens++;
-                    tot_auferido += atof(rides_cat[i].tip);
+                    tot_auferido += atoi(rides_cat[i].distance) * taxa_dist + taxa_base;
                 }
             }
             // Se o número de viagens for 0, o divisor será 0, logo iria dar erro. Logo, a avaliação média é imediatamente 0
@@ -44,6 +55,7 @@ void query1 (int counter, Driver *drivers_cat, User *users_cat, Ride *rides_cat,
                 if (strcmp(users_cat[i].user, id) == 0) q1_u = users_cat[i];
             }
            
+            int taxa_dist, taxa_base;
             char avaliacao_media[15]; int total_avaliacoes = 0; double av_media = 0;
             char numero_viagens[10]; int num_viagens = 0;
             char total_gasto[15]; double tot_gasto = 0;
@@ -53,7 +65,13 @@ void query1 (int counter, Driver *drivers_cat, User *users_cat, Ride *rides_cat,
                 if (strcmp(rides_cat[i].user, id) == 0) {
                     total_avaliacoes += atoi(rides_cat[i].score_user);
                     num_viagens++;
-                    tot_gasto += atof(rides_cat[i].tip);
+                    if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].car_class, "basic") == 0) taxa_base = 3.25;
+                        else if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].car_class, "green") == 0) taxa_base = 4;
+                        else if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].car_class, "premium") == 0) taxa_base = 5.2;
+                    if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].car_class, "basic") == 0) taxa_dist = 0.62;
+                        else if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].car_class, "green") == 0) taxa_dist = 0.79;
+                        else if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].car_class, "premium") == 0) taxa_dist = 0.94;
+                    tot_gasto += atoi(rides_cat[i].distance) * taxa_dist + taxa_base;
                 }
             }
             // Se o número de viagens for 0, o divisor será 0, logo iria dar erro. Logo, a avaliação média é imediatamente 0
