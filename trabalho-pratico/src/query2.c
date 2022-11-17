754cc3b4d4aa97165a3505a8fee1b0b08ba62c89
a100705 <a100705@alunos.uminho.pt>
2022-11-17 17:52:26 +0000

    Q2 - Ignorar Inactives

--- trabalho-pratico/src/query2.c
+++ trabalho-pratico/src/query2.c
@@ -7,11 +7,6 @@
 
 #include "../include/query2.h"
 
-/* TO DO - ORDEM DE DESENPATE:
-    1 - VIAGEM MAIS RECENTE
-    2 - ID DO DRIVER POR ORDEM CRESCENTE
-*/
-
 void query2 (int counter, Driver *drivers_cat, User *users_cat, Ride *rides_cat, char *N_arg) {
     int N = atoi(N_arg);                                                      // Converter argumento N string para int
 
@@ -20,10 +15,12 @@ void query2 (int counter, Driver *drivers_cat, User *users_cat, Ride *rides_cat,
     int num_viagens[10001];
     double av_med[10001], av_med_cpy[10001];
 
-    for (int i = 1; i <= 1000000; i++) {                                              // Percorrer o catálogo das rides
-        int ind_driver = atoi(rides_cat[i].driver);                                   // Calcular índice do driver pretendido
-        tot_avaliacoes[ind_driver] += atof(rides_cat[i].score_driver);                // Aumentar o total de avaliações do driver pretendido
-        num_viagens[ind_driver]++;                                                    // Incrementar o número de viagens do driver pretendido
+    for (int i = 1; i <= 1000000; i++) {                                                  // Percorrer o catálogo das rides
+        if (strcmp(drivers_cat[atoi(rides_cat[i].driver)].status, "active") == 0) {       // Verificar se o Driver está ativo
+            int ind_driver = atoi(rides_cat[i].driver);                                   // Calcular índice do driver pretendido
+            tot_avaliacoes[ind_driver] += atof(rides_cat[i].score_driver);                // Aumentar o total de avaliações do driver pretendido
+            num_viagens[ind_driver]++;                                                    // Incrementar o número de viagens do driver pretendido
+        }
     }
 
     for (int i = 1; i <= 10000; i++) av_med[i] = tot_avaliacoes[i] / num_viagens[i];  // Calcular a avaliação média para cada Driver armazenando na array av_med
