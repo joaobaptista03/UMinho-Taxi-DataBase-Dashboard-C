eae50c9a92c67e744a2dda37e40cf99e1b438737
a100705 <a100705@alunos.uminho.pt>
2022-11-20 20:27:26 +0000

    Q4 feita

--- trabalho-pratico/include/query4.h
+++ trabalho-pratico/include/query4.h
@@ -9,12 +9,13 @@
 #define QUERY4_H
 
 #include "catalog.h"
+#include "outputs.h"
 
 /**
  *
  * Este é o header da função que executa a Query 4, dado um ID em forma de string, e um contador para dizer em que linha está.
  * 
  */
-void query4 (int counter, Driver *drivers_cat, Ride *rides_cat, char *id);
+void query4 (int counter, Driver *drivers_cat, Ride *rides_cat, char *cidade);
 
 #endif
--- trabalho-pratico/src/query4.c
+++ trabalho-pratico/src/query4.c
@@ -7,6 +7,52 @@
 
 #include "../include/query4.h"
 
-void query4 (int counter, Driver *drivers_cat, Ride *rides_cat, char *id) {
+void query4 (int counter, Driver *drivers_cat, Ride *rides_cat, char *cidade) {
+    printf("A executar Q4 (linha de input %i)\n", counter);
 
+    double preco = 0;
+    int contagem = 0;
+
+    for (int i = 1; i < 1000000; i++) {
+        if (strcmp(rides_cat[i].city, cidade) == 0) {
+            int distance = atoi(rides_cat[i].distance);
+            char *class; class = malloc(10 * sizeof(char));
+            strcpy(class, drivers_cat[atoi(rides_cat[i].driver)].car_class);
+
+            float taxa_base, taxa_dist;
+            if (strcmp(class, "basic") == 0) {
+                taxa_base = 3.25;
+                taxa_dist = 0.62;
+            }
+            else if (strcmp(class, "green") == 0) {
+                taxa_base = 4;
+                taxa_dist = 0.79;
+            }
+            else if (strcmp(class, "premium") == 0) {
+                taxa_base = 5.2;
+                taxa_dist = 0.94;
+            }
+
+            preco += taxa_dist * distance + taxa_base;
+            contagem++;
+
+            free(class);
+        }
+    }
+
+    char *output; output = malloc(100 * sizeof(char));
+    
+    if (contagem == 0) {
+        handle_outputs(counter, "");
+        printf("Fim da Q4 (linha de input %i)\n", counter);
+        return;
+    }
+    
+    double resultado = preco/contagem;
+    sprintf(output, "%.3f\n", resultado);
+    handle_outputs(counter,output);
+
+    free(output);
+
+    printf("Fim da Q4 (linha de input %i)\n", counter);
 }
