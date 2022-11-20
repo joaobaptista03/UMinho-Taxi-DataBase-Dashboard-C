7df1b490989ca94f9b796979c1e87f7560ed6da8
a100705 <a100705@alunos.uminho.pt>
2022-11-20 19:49:43 +0000

    array dinamica input

--- trabalho-pratico/src/inputs.c
+++ trabalho-pratico/src/inputs.c
@@ -25,12 +25,12 @@ void handle_inputs(Driver *drivers_cat, User *users_cat, GHashTable *users_hash,
     }
     g_hash_table_foreach(users_hash, new_hash, NULL);
     g_hash_table_destroy(users_hash);
-    
+
     int counter = 1;                                                    // Contador do input em que está
-    char input[100];                                                    // String para qual cada linha de input irá ser copiada
+    char *input; input = malloc(100 * sizeof(char));                    // String para qual cada linha de input irá ser copiada
     while(fgets(input, 100, inputs)) {                                  // Loop While que irá fazer certas coisas (em cada loop) a cada linha do ficheiros de inputs  
         if (strchr(input, '\n')) *(strchr(input, '\n') - 1) = '\0';     // Trocar '\n' por '\0' na string input
-        char new_input[100];
+        char *new_input; new_input = malloc(100 * sizeof(char)); 
         strcpy(new_input, input);                                       // Encapsulamento
         if (new_input[0] == '1') query1(counter, drivers_cat_dup, users_cat_dup, users_hash_dup, rides_cat_dup, new_input + 2);                // Chamar a query1 se for o caso
         if (new_input[0] == '2') query2(counter, drivers_cat_dup, users_cat_dup, rides_cat_dup, new_input + 2);                            // Chamar a query2 se for o caso
@@ -44,10 +44,12 @@ void handle_inputs(Driver *drivers_cat, User *users_cat, GHashTable *users_hash,
         if (new_input[0] == '9') query9(counter, drivers_cat_dup, users_cat_dup, rides_cat_dup, new_input + 2, input + 13);
         */
        counter++;                                                       // Incrementar nº de inputs
+       free(new_input);
     }
 
     g_hash_table_destroy(users_hash_dup);
 
+    free(input);
     free(drivers_cat_dup);
     free(users_cat_dup);
     free(rides_cat_dup);
