b65b9bf740a5781260a30700c9aa8ac4cf2d65a9
a100705 <a100705@alunos.uminho.pt>
2022-11-21 01:15:53 +0000

    utimo commit revertido

--- trabalho-pratico/src/catalog.c
+++ trabalho-pratico/src/catalog.c
@@ -14,7 +14,7 @@ void inserir_dados(FILE *drivers, FILE *users, FILE *rides, FILE *inputs) {
     // Criar catálogo dos Drivers
     Driver *drivers_cat; drivers_cat = malloc(10001*sizeof(Driver));
 
-    for(int i = 0; fgets(temp, 1000, drivers); i++) {
+    for(int i = 1; fgets(temp, 1000, drivers); i++) {
         Driver temp_d;
         sscanf(temp, "%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^\n]", 
             temp_d.id, temp_d.name, temp_d.birth_date, temp_d.gender, temp_d.car_class, temp_d.license_plate, temp_d.city, temp_d.acc_creation, temp_d.status);
@@ -28,7 +28,7 @@ void inserir_dados(FILE *drivers, FILE *users, FILE *rides, FILE *inputs) {
 
     GHashTable *users_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 
-    for(int i = 0; fgets(temp, 1000, users); i++) {
+    for(int i = 1; fgets(temp, 1000, users); i++) {
         User temp_u;
         sscanf(temp, "%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^\n]", 
             temp_u.user, temp_u.name, temp_u.gender, temp_u.birth_date, temp_u.acc_creation, temp_u.pay_method, temp_u.status);
@@ -42,7 +42,7 @@ void inserir_dados(FILE *drivers, FILE *users, FILE *rides, FILE *inputs) {
     // Criar catálogo das Rides
     Ride *rides_cat; rides_cat = malloc(1000001*sizeof(Ride));
 
-    for(int i = 0; fgets(temp, 1000, rides); i++) {
+    for(int i = 1; fgets(temp, 1000, rides); i++) {
         Ride temp_r;
         sscanf(temp, "%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^\n]", 
             temp_r.id, temp_r.date, temp_r.driver, temp_r.user, temp_r.city, temp_r.distance, temp_r.score_user, temp_r.score_driver, temp_r.tip, temp_r.comment);
