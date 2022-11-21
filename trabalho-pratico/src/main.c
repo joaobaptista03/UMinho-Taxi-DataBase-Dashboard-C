3d415a7e3d4b4db6e1327e97ba38382b0fb4a122
a100705 <a100705@alunos.uminho.pt>
2022-11-21 20:45:19 +0000

    adicionado módulo files

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "../include/catalog.h"
+#include "../include/files.h"
 
 int main(int argc, char *argv[]) {
 
@@ -17,55 +17,16 @@ int main(int argc, char *argv[]) {
     }
     else puts("Número de argumentos válido! (3)");
 
+    system("mkdir Resultados");                                               // Criar diretório para os Resultados
+
     // Medição de tempo
     clock_t start, end;
     double cpu_time_used;
     start = clock();
 
-    FILE *drivers;
-        char *driverspath;
-        driverspath = (char *) malloc(1000 * sizeof(char));
-        strcpy(driverspath, argv[1]);
-        strcat(driverspath, "/drivers.csv");
-        drivers = fopen(driverspath, "r");
-        free(driverspath);
-        if (drivers != NULL) puts("Ficheiro dos Drivers aberto");
-        else puts("Erro na abertura do ficheiro dos Drivers");
-    FILE *users;
-        char *userspath;
-        userspath = (char *) malloc(1000 * sizeof(char));
-        strcpy(userspath, argv[1]);
-        strcat(userspath, "/users.csv");
-        users = fopen(userspath, "r");
-        free(userspath);
-        if (users != NULL) puts("Ficheiro dos Users aberto");
-        else puts("Erro na abertura do ficheiro dos Users");
-    FILE *rides;
-        char *ridespath;
-        ridespath = (char *) malloc(1000 * sizeof(char));
-        strcpy(ridespath, argv[1]);
-        strcat(ridespath, "/rides.csv");
-        rides = fopen(ridespath, "r");
-        free(ridespath);
-        if (rides != NULL) puts("Ficheiro das Rides aberto");
-        else puts("Erro na abertura do ficheiro das Rides");
-    FILE *inputs;
-        inputs = fopen(argv[2], "r");
-        if (inputs != NULL) puts("Ficheiro dos Inputs aberto");
+    open_files(argv[1], argv[2]);
 
-    system("mkdir Resultados");                                               // Criar diretório para os Resultados
     puts("");
-
-    inserir_dados(drivers, users, rides, inputs);                             // Inserir dados nos catálogos
-
-    fclose(drivers);
-        if (drivers != NULL) puts("Ficheiro dos Drivers fechado");
-    fclose(rides);
-        if (rides != NULL) puts("Ficheiro dos Rides fechado");
-    fclose(users);
-        if (users != NULL) puts("Ficheiro dos Users fechado");
-    fclose(inputs);
-        if (inputs != NULL) puts("Ficheiro dos Inputs fechado\n");
 
     // Medição de tempo
     end = clock();
