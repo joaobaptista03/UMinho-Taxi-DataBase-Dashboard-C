3cb11d846a074374f8bc6c4f73a976b39b07f4a3
a100896 <a100896@alunos.uminho.pt>
2023-01-23 22:04:25 +0000

    inicio query 9

---
+++ trabalho-pratico/include/query9.h
@@ -0,0 +1,25 @@
+/**
+ * @file query9.h
+ * 
+ * Este ficheiro contém as assinaturas das funções relacionadas com as Query 9.
+ * 
+ */
+
+#ifndef QUERY9_H
+#define QUERY9_H
+
+#include "cat_drivers.h"
+#include "cat_rides.h"
+#include "cat_users.h"
+#include "randomfuncs.h"
+#include "outputs.h"
+
+/**
+ *
+ * Este é o header da função que executa a Query 9, dado um ID em forma de string, e um contador para dizer em que linha está.
+ * 
+ */
+void query9 (int counter, Driver *drivers_cat, GHashTable *drivers_hash, User *users_cat, GHashTable *users_hash, Ride *rides_cat, char *input, int mode);
+
+
+#endif
---
+++ trabalho-pratico/src/query9.c
@@ -0,0 +1,8 @@
+/**
+ * @file query9.c
+ * 
+ * Este ficheiro contém o conteúdo das funções relacionadas com as Query 9.
+ * 
+ */
+
+#include "../include/query9.h"
